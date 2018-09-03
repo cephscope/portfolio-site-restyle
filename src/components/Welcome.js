cfe5cfb4c6d4667c935a1c8aae1336a2a1b322c3
cephscope <sworm.null@gmail.com>
2018-09-03 15:11:45 -0400

    added scroll-down animation

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,10 +8,16 @@ const Welcome = () => {
         <div id="welcome">
           <h1>Hi, I'm Roy.</h1>
           <p>I'm a front-end developer living in New York City. I really like making stuff on the internet using emerging web technologies.</p>
-          <br />
-          <p className="section-link"><a className="section-link" href="https://github.com/rycthy" target="_blank">GitHub</a></p>
-          <p className="section-link"><a className="section-link" href="mailto:rccathey@gmail.com">Email</a></p>
-          <p className="section-link"><a className="section-link" href="https://www.linkedin.com/in/roy-cathey/" target="_blank">LinkedIn</a></p>
+          <div id="contact-links">
+            <p className="section-link"><a className="section-link" href="https://github.com/rycthy" target="_blank">GitHub</a></p>
+            <p className="section-link"><a className="section-link" href="mailto:rccathey@gmail.com">Email</a></p>
+            <p className="section-link"><a className="section-link" href="https://www.linkedin.com/in/roy-cathey/" target="_blank">LinkedIn</a></p>
+          </div>
+        </div>
+        <div class="scroll-down">
+          <div class="chevron"></div>
+          <div class="chevron"></div>
+          <div class="chevron"></div>
         </div>
       </section>
       <WorkPage />
