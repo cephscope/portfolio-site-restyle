423f04f6cc57d2acff21e453ebf5eed995c193ab
cephscope <sworm.null@gmail.com>
2018-09-03 00:13:32 -0400

    moved projects to landing page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import WorkPage from './WorkPage';
 
 const Welcome = () => {
   return (
-    <section>
-      <div id="welcome">
-        <h1>Hi, I'm Roy.</h1>
-        <p>I'm a front-end developer living in New York City. I really like making stuff on the internet using emerging web technologies.</p>
-        <br />
-        <p className="section-link"><a className="section-link" href="https://github.com/rycthy" target="_blank">GitHub</a></p>
-        <p className="section-link"><a className="section-link" href="mailto:rccathey@gmail.com">Email</a></p>
-        <p className="section-link"><a className="section-link" href="https://www.linkedin.com/in/roy-cathey/" target="_blank">LinkedIn</a></p>
-      </div>
-    </section>
+    <div>
+      <section>
+        <div id="welcome">
+          <h1>Hi, I'm Roy.</h1>
+          <p>I'm a front-end developer living in New York City. I really like making stuff on the internet using emerging web technologies.</p>
+          <br />
+          <p className="section-link"><a className="section-link" href="https://github.com/rycthy" target="_blank">GitHub</a></p>
+          <p className="section-link"><a className="section-link" href="mailto:rccathey@gmail.com">Email</a></p>
+          <p className="section-link"><a className="section-link" href="https://www.linkedin.com/in/roy-cathey/" target="_blank">LinkedIn</a></p>
+        </div>
+      </section>
+      <WorkPage />
+    </div>
   );
 }
 
