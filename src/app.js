30c9f6d5e06fff97cc67281246e220cc8e352379
cephscope <sworm.null@gmail.com>
2018-10-10 22:01:59 -0400

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
+// import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import Welcome from './components/Welcome';
 
 const jsx = (
-  <AppRouter />
+  <Welcome />
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import WorkPage from './WorkPage';
+import Frame from './Frame';
 import { Projects } from './Projects';
 
 const Welcome = () => {
   return (
     <div>
+      <Frame />
+      <div id="container">
+      <main>
       <section>
         <div id="welcome">
           <h2>THIS IS A WEBSITE ABOUT</h2>
@@ -24,36 +27,10 @@ const Welcome = () => {
         </div>
       </section>
       <Projects />
+      </main>
+      </div>
     </div>
   );
 }
 
 export default Welcome;
-// <h3>Core Development Toolset</h3>
-//           <div className="row-list" id="toolset">
-//             <span>
-//               <i className="fab fa-js"></i>
-//               <p>JavaScript</p>
-//             </span>
-//             <span>
-//               <i className="fab fa-html5"></i>
-//               <p>HTML5</p>
-//             </span>
-//             <span>
-//               <i className="fab fa-css3"></i>
-//               <p>CSS3</p>
-//             </span>
-//             <span>
-//               <i className="fab fa-git"></i>
-//               <p>git</p>
-//             </span>
-//             <span>
-//               <i className="fab fa-react"></i>
-//               <p>React.js</p>
-//             </span>
-//           </div>
-//           <div className="scroll-down">
-//             <div className="chevron"></div>
-//             <div className="chevron"></div>
-//             <div className="chevron"></div>
-//           </div>
